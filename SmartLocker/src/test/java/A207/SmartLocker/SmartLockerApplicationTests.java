36fa8abf664e915a1739ea567a288964edb7a4cc
zzzz955 <zzzzz955@gmail.com>
2025-01-20 14:54:38 +0900

    feat#64

---
+++ SmartLocker/src/test/java/A207/SmartLocker/SmartLockerApplicationTests.java
@@ -0,0 +1,13 @@
+package A207.SmartLocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
